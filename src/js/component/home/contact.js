a682c4ddd5d71578b65f1ef9b1eba5e29b020b25
Jeeva <jeeva@tricog.com>
2019-08-18 17:37:51 +0530

    add foter

--- src/js/component/home/contact.js
+++ src/js/component/home/contact.js
@@ -3,15 +3,15 @@ import React, { Component } from "react";
 export default class Contacts extends Component{
     render(){
         return(
-            <div className="footer_container contact">
-            <div className="helper">
-                <span className="header">
+            <div className="">
+            <div className="">
+                <span className="">
                     How can we help you?
 				</span>
-                <span className="details">
-                    Send us email at <span className="det_contact">jeevaramu97@gmail.com</span>
+                <span className="">
+                    Send us email at <span className="">jeevaramu97@gmail.com</span>
                     <br />
-                    Or call us at <span className="det_contact">9688669002</span>
+                    Or call us at <span className="">9688669002</span>
                 </span>
             </div>
         </div>
