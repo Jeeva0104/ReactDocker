84113dfd2793c01076279729fce5b50641e1cfdb
Jeeva <jeeva@tricog.com>
2019-08-18 17:41:42 +0530

    set

--- src/js/component/home/gallery.js
+++ src/js/component/home/gallery.js
@@ -4,7 +4,7 @@ export default class Gallery extends React.Component{
     render(){
         return (
             <div>
-                Gallery Page
+                under consider construction
             </div>
         )
     }
